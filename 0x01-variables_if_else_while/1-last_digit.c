f8a0249f738bf1beeed3581442130e3ea640994c
Kveltri22 <kveltri13@gmail.com>
2023-01-28 13:56:31 -0800

    k git ad .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 != 0 && n % 10 < 6)
 	{
-	   printf("Last digit of %d is %d is less than 6 and not 0\n", n, % 10);
+	   printf("Last digit of %d is %d is less than 6 and not 0\n", n % 10);
 	}
 	else if (n % 10 > 5)
 	{
