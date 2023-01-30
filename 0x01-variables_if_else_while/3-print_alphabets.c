47b7691985616530228bf18e296ff6b6a5ff0ea2
Kveltri22 <kveltri13@gmail.com>
2023-01-30 05:29:20 -0800

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* Main - Entry point
+* Return: Alway 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+
+	{
+		putchar(c);
+	}
+
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar ('\n');
+	return (0);
+}
