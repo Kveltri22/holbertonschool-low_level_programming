e8e83e9b9d9f7b3fed90b8036bb3c2364d17ebf1
Kveltri22 <kveltri13@gmail.com>
2023-01-30 06:58:50 -0800

    j

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main: description code to print all signle digit numbers no char command
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+
+{
+putchar (c);
+}
+putchar('\n');
+return (0);
+}
