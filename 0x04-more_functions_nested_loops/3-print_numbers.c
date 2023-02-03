f2b69a2198588e92eae58d2481053f7f4f4ec91e
Kveltri22 <kveltri13@gmail.com>
2023-02-03 11:06:19 -0800

    f

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_number - print number 1-9
+* @c: char
+* Return: 0
+*/
+
+void print_numbers(void)
+
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar ('c');
+		c++;
+	}
+	_putchar ('\n');
+}
