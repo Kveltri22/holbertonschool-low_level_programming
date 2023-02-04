57998910f370bc184410cd2077810a6d4c1c234e
Kveltri22 <kveltri13@gmail.com>
2023-02-03 19:08:21 -0800

    h

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_diagonal - print a line
+* @n: legnth of line
+* Return: 0
+*/
+
+void print_diagonal(int n)
+{
+	while (!(n <= 0))
+	{
+		_putchar('\\');
+		_putchar('\n');
+		n++;
+}
+		_putchar('\n');
+
+}
