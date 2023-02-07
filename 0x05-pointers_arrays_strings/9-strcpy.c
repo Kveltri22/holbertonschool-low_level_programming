419e7e319110ddacfcc7a1f7d9a7bc560ee1c390
Kveltri22 <kveltri13@gmail.com>
2023-02-06 19:19:17 -0800

    h

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,19 @@
 /**
-* _strcpy - fuction copies the string pointed
-* @dest: pointer to copy the string to.
-* @src: the string to copy to the destination.
-*
-* Return: a copy of the src.
+* *_strcpy - copies string pointed to by src to buffer pointed to by dest
+* @dest: the pointer to a string
+* @src: the pointer to a buffer
+* Return: the value of the pointer to dest.
 */
-
 char *_strcpy(char *dest, char *src)
-
 {
-	int index = 0;
+		int i;
 
-	while (*src != '\0')
-	{
-	dest[index] = *src;
-	index++;
-	src++;
-	}
-	return (dest);
+		i = 0;
+			while (src[i] != '\0')
+			{
+			dest[i] = src[i];
+			i++;
+			}
+				dest[i] = '\0';
+					return (dest);
 }
