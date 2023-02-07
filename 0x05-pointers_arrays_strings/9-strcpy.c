193addd62a51354b5898e5075110675714828ea2
Kveltri22 <kveltri13@gmail.com>
2023-02-06 19:32:47 -0800

    h

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,25 @@
-/**
-* *_strcpy - copies string pointed to by src to buffer pointed to by dest
-* @dest: the pointer to a string
-* @src: the pointer to a buffer
-* Return: the value of the pointer to dest.
-*/
-char *_strcpy(char *dest, char *src)
-{
-		int i;
+#include "main.h"
+#include <stdio.h>
 
-		i = 0;
-			while (src[i] != '\0')
-			{
-			dest[i] = src[i];
-			i++;
-			}
-				dest[i] = '\0';
-					return (dest);
+/**
+* print_rev -prints a string in reverse
+* @s: a string
+*
+* Return: string in reverse
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+	 i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		putchar(s[i]);
+	}
+	putchar('\n');
 }
