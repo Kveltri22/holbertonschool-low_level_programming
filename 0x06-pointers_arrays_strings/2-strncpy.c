99cf46f7ce21bb1accff01e8cf9c5f72b7a4cdeb
Kveltri22 <kveltri13@gmail.com>
2023-02-13 15:07:22 -0800

    h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strncpy - concantenate strings
+* @dest: Destination
+* @src: Src String
+* @n: Int n
+* Return: Pointer to string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int s = 0;
+
+	while (src[s] != 0 && s < n)
+	{
+		dest[s] = src[s];
+		s++;
+	}
+	while (s < n)
+	{
+		dest[s] = 0;
+		s++;
+	}
+
+	return (dest);
+}
