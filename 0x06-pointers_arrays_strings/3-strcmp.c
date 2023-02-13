5a4530000b42e160c8210240e8a45377c500736d
Kveltri22 <kveltri13@gmail.com>
2023-02-13 15:13:40 -0800

    g

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcmp - compare 2 strings
+* @s1: String one
+* @s2: string two
+* Return: pointer to resulting string
+*/
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int i = 0;
+
+	int dif = 0;
+
+	while (s1[1] != 0 && s2[i] != 0)
+	{
+		dif = s1[i] - s2[i];
+		if (dif != 0)
+		break;
+		i++;
+	}
+
+		return (dif);
+}
