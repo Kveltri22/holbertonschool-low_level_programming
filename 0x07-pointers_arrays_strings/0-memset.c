ab5a054a12afa923f97eb3e660d9c1609c1f3540
Kveltri22 <kveltri13@gmail.com>
2023-02-13 18:29:44 -0800

    you

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* *_memset - fill memory with bytes
+* @s: Char
+* @b: Char
+* @n: not chosen
+* Return: Final Char
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+		for (i = 0; i < n; i++)
+			s[i] = b;
+		return (s);
+}
