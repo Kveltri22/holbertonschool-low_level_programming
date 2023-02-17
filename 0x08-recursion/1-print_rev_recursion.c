467df88f4d0007529036573bc5460f1ef6f57ecd
Kveltri22 <kveltri13@gmail.com>
2023-02-17 14:13:23 -0800

    sss

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - Get string length
+* @s: char
+* Return: Length of String (*s)
+*/
+
+int _strlen_recursion(char *s)
+{
+	int num = 0;
+
+	if (*s)
+	{
+		num++;
+		num += _strlen_recursion(s + 1);
+	}
+
+	return (num);
+}
