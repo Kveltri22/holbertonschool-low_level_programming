2702d7c8f03cca963ff6588d4febaf402484f4d3
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-02-20 12:45:57 -0600

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+2	
+3	/**
+4	* _strcat - Concatenate
+5	* @dest: Destination
+6	* @src: src String
+7	* Return: Final String
+8	*/
+9	char *_strcat(char *dest, char *src)
+10	{
+11		int a = 0, b = 0;
+12	
+13		while (dest[a] != 0)
