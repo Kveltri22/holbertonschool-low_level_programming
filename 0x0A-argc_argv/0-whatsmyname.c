dca5608e67ee8fb298e6abf5fd4cc576c6dee569
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-02-20 15:25:25 -0600

    Update 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - display name
+ * @argv: name of app
+ * @argc: size of argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc--;
+	printf("%s\n", argv[0]);
+	return (0);
+}
