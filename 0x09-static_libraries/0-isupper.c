207f2ef4641ffd9b3713eec6a8004971c75870c8
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-02-20 12:43:51 -0600

    Create 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+2	/**
+3	*  _isupper - a function that checks for uppercase character.
+4	*
+5	* @c:int to be checked
+6	*
+7	* Return:1 if true 0 if false
+8	*/
+9	
+10	int _isupper(int c)
+11	{
+12		if (c >= 'A' && c <= 'Z')
+13		{
+14			return (1);
+15		}
+16		else
+17		{
+18			return (0);
+19		}
+20	
+21	}
