c1174677e103d2b8f3416bba48a84ebcabfd4804
Kveltri22 <kveltri13@gmail.com>
2023-02-20 11:46:55 -0800

    library

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
-2	
-3	/**
-4	* *_memset - fill memory with bytes
-5	* @s: Char
-6	* @b: Char
-7	* @n: not chosen
-8	* Return: Final Char
-9	*/
-10	
-11	char *_memset(char *s, char b, unsigned int n)
-12	
-13	{
-14		unsigned int i;
-15	
-16			for (i = 0; i < n; i++)
-17				s[i] = b;
-18			return (s);
-19	}
+	
+	/**
+	* *_memset - fill memory with bytes
+	* @s: Char
+	* @b: Char
+	* @n: not chosen
+	* Return: Final Char
+	*/
+	
+	char *_memset(char *s, char b, unsigned int n)
+	
+	{
+		unsigned int i;
+	
+			for (i = 0; i < n; i++)
+				s[i] = b;
+			return (s);
+	}
--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,13 +1,22 @@
 #include "main.h"
-2	
-3	/**
-4	* _strcat - Concatenate
-5	* @dest: Destination
-6	* @src: src String
-7	* Return: Final String
-8	*/
-9	char *_strcat(char *dest, char *src)
-10	{
-11		int a = 0, b = 0;
-12	
-13		while (dest[a] != 0)
+
+/**
+   * _strcpy - hjk
+    * @dest: pointer
+     * @src: copy to desitnation
+      * Return: a copy of src
+       */
+
+char *_strcpy(char *dest, char *src)
+{
+		int i;
+
+			i = 0;
+				while (src[i] != '\0')
+						{
+									dest[i] = src[i];
+											i++;
+												}
+					dest[i] = '\0';
+						return (dest);
+}
