0077a6c7ce3dbc96bb855909ecf1374f22f631ee
Kveltri22 <kveltri13@gmail.com>
2023-02-22 11:55:07 -0800

    h

---
+++ 0x0D-preprocessor/mian.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+/**
+   * main - print file name
+    *
+     * Return:void
+      */
+int main(void)
+{
+		printf("%s\n", __FILE__);
+			return (0);
+}
+
+
+#endif
