95e2f06c9fcd2dbe76bff5cd1bd45aeaffbcfd74
Kveltri22 <kveltri13@gmail.com>
2023-03-02 15:00:01 -0800

    h

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* str_concat - concatenates two strings
+* @s1: second string
+* @s2: second string 
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# holbertonschool-low_level_programming
-3rd Sprint
-Here we go  
+Static Library 
+
