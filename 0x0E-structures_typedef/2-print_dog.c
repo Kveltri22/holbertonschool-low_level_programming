ea7b7047bf435d824184e1b946a718c0a1b0c868
Kveltri22 <kveltri13@gmail.com>
2023-03-06 08:34:33 -0800

    h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* print_dog - printing dog depending on the input
+* @d: Print a structure dog
+* Return: Always 0
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, d->owner);
+}
