c8918c8b9ed28cd93b4cb1aa67edc5c6d099ae39
Kveltri22 <kveltri13@gmail.com>
2023-03-06 20:26:43 -0800

    a

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+*print_numbers - prototype to print numbers
+*@separator: separator variable
+*@n: variable
+*Return: 0
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ..
+}
+	unsigned int i = 0;
+	va_list list;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(list, n);
+	while (i < n - 1)
+	{
+		printf("%d", va_arg(list, int));
+		if (separator != NULL)
+			printf("%s", separator);
+		i++;
+	}
+	printf("%d\n", va_arg(list, int));
+}
