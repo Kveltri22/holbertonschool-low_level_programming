a687e599b901e832e3d6e002dd27b8ed4ebd1c35
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-03-06 22:41:05 -0600

    Update 2-print_strings.c

--- 0x10-variadic_functions/#include "variadic_functions.h"/**  *print_strings - a loom prints strings  *@separator: separates  *@n: i do declare  *Return: nothing im quite sure  */2-print_strings.c
+++ 0x10-variadic_functions/#include "variadic_functions.h"/**  *print_strings - a loom prints strings  *@separator: separates  *@n: i do declare  *Return: nothing im quite sure  */2-print_strings.c
@@ -1,38 +1,46 @@
 #include "variadic_functions.h"
-/**
- *print_strings - prints strings
- *@separator: separator variable
- *@n: variable to declare
- *Return: 0
- */
+	#include <stdio.h>
+	#include <stdarg.h>
 
-void print_strings(const char *separator, const unsigned int n, ...)
-{
-	unsigned int i = 0;
-	va_list list;
-	char *str;
 
-	if (n == 0)
+	/**
+	 * print_strings - Prints strings, followed by a new line.
+	 * @separator: The string to be printed between strings.
+	 * @n: The number of strings passed to the function.
+	 * @...: A variable number of strings to be printed.
+	 *
+	 * Description: If separator is NULL, it is not printed.
+	 *              If one of the strings if NULL, (nil) is printed instead.
+	 */
+	void print_strings(const char *separator, const unsigned int n, ...)
 	{
+		va_list strings;
+		char *str;
+		unsigned int index;
+
+
+		va_start(strings, n);
+
+
+		for (index = 0; index < n; index++)
+		{
+			str = va_arg(strings, char *);
+
+
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+
+
+			if (index != (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
+
+
 		printf("\n");
-		return;
+
+
+		va_end(strings);
 	}
 
-	va_start(list, n);
-	while (i < n - 1)
-	{
-		str = va_arg(list, char *);
-		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
-		if (separator != NULL)
-			printf("%s", separator);
-		i++;
-	}
-	str = va_arg(list, char *);
-	if (str == NULL)
-		printf("(nil)\n");
-	else
-		printf("%s\n", str);
-}
