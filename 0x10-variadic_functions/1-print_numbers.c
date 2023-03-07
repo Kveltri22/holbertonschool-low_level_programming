ea996545fd747e410cbacb1ff6ea76249d103d7d
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-03-06 22:29:57 -0600

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,13 +1,13 @@
 #include "variadic_functions.h"
 /**
-*print_numbers - prototype to print numbers
-*@separator: separator variable
-*@n: variable
-*Return: 0
-*/
+ *print_numbers - print numbers
+ *@separator: separator variable
+ *@n: variable
+ *Return: 0
+ */
 
-void print_numbers(const char *separator, const unsigned int n, ..
-}
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
 	unsigned int i = 0;
 	va_list list;
 
