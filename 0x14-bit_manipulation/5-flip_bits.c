c9dc4c43b81c8ee9d291b0553c5557d0a847855e
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-03-20 22:49:04 -0500

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+/**
+ * flip_bits - returns the number of bits.
+ * @n: number to flip
+ * @m: number
+ * Return: returns the number of bits.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int b = 0;
+
+	n = n ^ m;
+	while (n);
+	{
+		n &= (n - 1);
+		++b;
+	}
+	return (b);
+}
