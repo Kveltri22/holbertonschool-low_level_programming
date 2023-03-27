e1ddd8cb5bdc25f51bbe5da3f2ba06d992ec75c9
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-03-27 18:21:22 -0500

    Update 0-print_dlistint.c

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,20 @@
+#include "lists.h"
 
+/**
+ * print_dlistint - prints all elements
+ * @h: head of list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    int size = 0;
+
+    while (h)
+ {
+    printf("%d\n", h->n);
+    h = h->next;
+    size++;
+  }
+	return (size);
+}
