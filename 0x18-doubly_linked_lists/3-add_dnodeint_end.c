b61ac5295b47bb3f146966d4784214319804e3cd
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-03-27 18:42:06 -0500

    Create 3-add_dnodeint_end.c

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node at end of dll
+ * Return: point node
+ * @head: node head
+ * @n: data
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *current;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (0);
+	if (!head)
+	{
+		free(new);
+		return (0);
+	}
+	current = *head;
+
+	new->n = n;
+	new->next = 0;
+	if (!*head)
+	{
+		new->prev = 0;
+		*head = new;
+		return (new);
+	}
+	while (current->next)
+		current = current->next;
+	new->prev = current;
+	current->next = new;
+	return (new);
+}
