e4a3487dd47635d061de9cb0e0babb8398064e89
Kristina <kveltri13@gmail.com>
2023-03-27 17:03:59 -0700

    h

--- 0x18-doubly_linked_lists/6-sum_dlistint.c
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- *sum_dlistint - lists the sum of int
- *@head: head list 
- *Return: sum of data bro
- */
+*sum_dlistint - lists the sum of int
+*@head: head list
+*Return: sum of data bro
+*/
 
 int sum_dlistint(dlistint_t *head)
 {
